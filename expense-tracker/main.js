119ffe4b69918a5a73b992ed37117a5fd76936d8
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-02-29 12:35:25 +0530

    added login component

--- expense-tracker/main.js
+++ expense-tracker/main.js
@@ -5,35 +5,35 @@ const path = require("path");
 let mainWindow
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
 
-    mainWindow.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `/dist/index.html`),
-            protocol: "file:",
-            slashes: true
-        })
-    );
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+	mainWindow.loadURL(
+		url.format({
+			pathname: path.join(__dirname, `/dist/index.html`),
+			protocol: "file:",
+			slashes: true
+		})
+	)
+	
+	mainWindow.webContents.openDevTools()
 
-    mainWindow.on('closed', function () {
-        mainWindow = null
-    })
+	mainWindow.on('closed', function () {
+		mainWindow = null
+	})
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+	if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-    if (mainWindow === null) createWindow()
+	if (mainWindow === null) createWindow()
 })
--- expense-tracker/src/app/app.module.ts
+++ expense-tracker/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'
 
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
